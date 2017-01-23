1bb9dffb4fb1e1bc6a00e435deea0d53fdd43a52
Nelson Segarra <junicus@outlook.com>
2017-01-23 11:34:32 -0400

    GrindSolution Start

---
+++ GrindSolution/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrindSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var iberdir = Environment.GetEnvironmentVariable("IBERDIR");
+            var grindPath = Path.Combine(iberdir, "BIN", "GRIND.EXE");
+            var dateFolder = DateTime.Today.AddDays(-1).ToString("yyyyMMdd");
+
+            var grindProcess = new Process();
+            try
+            {
+                grindProcess.StartInfo.UseShellExecute = false;
+                grindProcess.StartInfo.FileName = grindPath;
+                grindProcess.StartInfo.Arguments = $"/DATE {dateFolder}";
+                grindProcess.StartInfo.CreateNoWindow = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
